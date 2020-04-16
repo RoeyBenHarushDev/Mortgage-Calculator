293e96a4ce2fcdd5a5fc673e2b3459963c8e746a
roey <roey1234r@gmail.com>
2020-04-16 20:51:59 +0300

    New Mortgage Calculator

--- Mortgage Calculator.js
+++ Mortgage Calculator.js
@@ -5,6 +5,7 @@
     let Payment = document.getElementById('Payment')
     let result = document.getElementById('result')
     let isOk = document.getElementById('isOk')
+    let input = document.querySelector('input')
 
 function calc ()
 {
@@ -21,16 +22,88 @@ function calc ()
     }
 }
 
-function checkInput(parameter,errorElement)
+function checkInput(event)
 {
-    let errorParagraph = document.getElementById(errorElement)
+    let value = Number( event.target.value )
+    
     
 
-    if ( isNaN(parameter) )
+    if ( isNaN(value) )
     {
-        errorParagraph.style.display = 'block'
+        FundAmount.style.border = '2px solid red'
+        document.getElementById('FundAmountError').innerHTML = '***נא להזין רק מספרים***'
     }
      else {
-        errorParagraph.style.display = 'none'
+        FundAmount.style.border = ''
+        document.getElementById('FundAmountError').innerHTML = ''
     }    
 }
+FundAmount.addEventListener(
+    'change',
+    checkInput
+)
+function checkInput2(event)
+{
+    let value = Number( event.target.value )
+    
+    
+
+    if ( isNaN(value) )
+    {
+        Interest.style.border = '2px solid red'
+        document.getElementById('InterestError').innerHTML = 'נא להזין רק מספרים'
+    }
+     else {
+        Interest.style.border = ''
+        document.getElementById('InterestError').innerHTML = ''
+    }    
+}
+Interest.addEventListener(
+    'change',
+    checkInput2
+)
+function checkInput3(event)
+{
+    let value = Number( event.target.value )
+    
+    
+
+    if ( isNaN(value) )
+    {
+        Years.style.border = '2px solid red'
+        document.getElementById('YearsError').innerHTML = 'נא להזין רק מספרים'
+    }
+     else {
+        Years.style.border = ''
+        document.getElementById('YearsError').innerHTML = ''
+    }    
+}
+Years.addEventListener(
+    'change',
+    checkInput3
+)
+
+
+function checkInput4(event)
+{
+    let value = Number( event.target.value )
+    
+    
+
+    if ( isNaN(value) )
+    {
+        Payment.style.border = '2px solid red'
+        document.getElementById('PaymentError').innerHTML = 'נא להזין רק מספרים'
+    }
+     else {
+        Payment.style.border = ''
+        document.getElementById('PaymentError').innerHTML = ''
+    }    
+}
+Payment.addEventListener(
+    'change',
+    checkInput4
+)
+
+
+
